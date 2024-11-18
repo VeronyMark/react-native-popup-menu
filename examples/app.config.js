1a7c31cfd5bb692f6d049c68cc278dbf39935a1c
VeronyMark <veronika.markova2002@gmail.com>
2024-11-18 17:28:58 +0100

    expo update

---
+++ examples/app.config.js
@@ -0,0 +1,37 @@
+module.exports = () => {
+  return {
+        name: "react-native-popup-menu-examples",
+        slug: "RNPopupMenuExamples",
+        privacy: "public",
+        platforms: ["ios", "android"],
+        version: "1.0.0",
+        owner: "instea.co",
+        orientation: "portrait",
+        icon: "./assets/icon.png",
+        splash: {
+          image: "./assets/splash.png",
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+        updates: {
+          fallbackToCacheTimeout: 0,
+        },
+        assetBundlePatterns: ["**/*"],
+        ios: {
+          supportsTablet: true,
+        },
+        android: {
+          package: "sk.instea.rnpopupmenuexamples",
+          versionCode: 1,
+          adaptiveIcon: {
+            foregroundImage: "./assets/splash.png",
+            backgroundColor: "#FFFFFF",
+          },
+        },
+         extra: {
+              eas: {
+                projectId: "bb486290-87aa-4799-84e2-fe1d0e93c74d",
+              },
+         },
+  };
+};
